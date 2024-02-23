cb5588ca6f29c16f8ccf05c085948b5c6d6f60bc
Tirthajyoti Ghosh <itirthahere@gmail.com>
2024-02-23 20:29:18 +0530

    Fix vercel deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import router from '../src/routes';
+import router from './routes';
 
 dotenv.config();
 
